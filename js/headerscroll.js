6d3cb6ec5e5412383c80b7fa14fc460254b6947d
Him Agrawal <himagrawal@Hims-MacBook-Air.local>
2023-03-03 22:52:18 +0530

    shankar code

--- js/headerscroll.js
+++
@@ -1,8 +0,0 @@
-$(window).scroll(function() {
-    if ($(this).scrollTop() > 1){  
-        $('header').addClass("scroll-custom");
-      }
-      else{
-        $('header').removeClass("scroll-custom");
-      }
-    });
