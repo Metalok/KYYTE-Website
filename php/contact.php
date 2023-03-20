9e1cda7f6cbebd6732a5e01c29080edd3303c585
mohan-metalok-19 <mohan@metalok.io>
2023-03-20 16:01:33 +0530

    Home page banner image, contact us heading update

--- php/contact.php
+++ php/contact.php
@@ -1,75 +1,42 @@
 <?php
-/*
- *  CONFIGURE EVERYTHING HERE
- */
+  use PHPMailer\PHPMailer\PHPMailer;
+  use PHPMailer\PHPMailer\Exception;
+  require 'PHPMailer-master/src/Exception.php';
+  require 'PHPMailer-master/src/PHPMailer.php';
+  require 'PHPMailer-master/src/SMTP.php';
 
-// an email address that will be in the From field of the email.
-$from = 'Kyyte contact form <demo@domain.com>';
+  $mail = new PHPMailer();
+  $mail->IsSMTP();
 
-// an email address that will receive the email with the output of the form
-$sendTo = 'mohan@metalok.io';
+  //$mail->SMTPDebug  = 0;  
+  $mail->SMTPAuth   = TRUE;
+  $mail->SMTPSecure = "tls";
+  $mail->Port       = 587;
+  $mail->Host       = "smtp.gmail.com";
+  $mail->Username   = "mohanachary5886@gmail.com";
+  $mail->Password   = "hnjlxvvkfnmwhnce";
 
-// subject of the email
-$subject = 'New message from contact form';
-
-// form field names and their translations.
-// array variable name => Text to appear in the email
-$fields = array('fullname' => 'Name','email' => 'Email', 'message' => 'Message'); 
-
-// message that will be displayed when everything is OK :)
-$okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
-
-// If something goes wrong, we will display this message.
-$errorMessage = 'There was an error while submitting the form. Please try again later';
-
-/*
- *  LET'S DO THE SENDING
- */
-
-// if you are not debugging and don't need error reporting, turn this off by error_reporting(0);
-error_reporting(E_ALL & ~E_NOTICE);
-
-try
-{
-
-    if(count($_POST) == 0) throw new \Exception('Form is empty');
-            
-    $emailText = "You have a new message from your contact form\n=============================\n";
-
-    foreach ($_POST as $key => $value) {
-        // If the field exists in the $fields array, include it in the email 
-        if (isset($fields[$key])) {
-            $emailText .= "$fields[$key]: $value\n";
-        }
-    }
-
-    // All the neccessary headers for the email.
-    $headers = array('Content-Type: text/plain; charset="UTF-8";',
-        'From: ' . $from,
-        'Reply-To: ' . $from,
-        'Return-Path: ' . $from,
-    );
-    
-    // Send email
-    mail($sendTo, $subject, $emailText, implode("\n", $headers));
-
-    $responseArray = array('type' => 'success', 'message' => $okMessage);
-}
-catch (\Exception $e)
-{
-    $responseArray = array('type' => 'danger', 'message' => $errorMessage);
-}
+  $mail->IsHTML(true);
+  $mail->AddAddress("recipient-email", "recipient-name");
+  $mail->SetFrom("mohanachary5886@gmail.com", "set-from-name");
+  $mail->AddReplyTo("reply-to-email", "reply-to-name");
+  $mail->AddCC("cc-recipient-email", "cc-recipient-name");
+  $mail->Subject = "Test is Test Email sent via Gmail SMTP Server using PHP Mailer";
+  $content = "<b>This is a Test Email sent via Gmail SMTP Server using PHP mailer class.</b>";
 
 
-// if requested by AJAX request return JSON response
-if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
-    $encoded = json_encode($responseArray);
 
-    header('Content-Type: application/json');
+    $mail->isHTML(true);  // Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-    echo $encoded;
-}
-// else just display the message
-else {
-    echo $responseArray['message'];
-}
+
+  $mail->MsgHTML($content); 
+  if(!$mail->Send()) {
+    echo "Error while sending Email.";
+    var_dump($mail);
+  } else {
+    echo "Email sent successfully";
+  }
+?>
